2fe7fec9e25f67f28e872031597f01df4a0347cd
يسوع المسيح <165551623+KiRiLldrs@users.noreply.github.com>
2025-02-18 19:50:51 +0400

    Add files via upload

--- Typescript/lab5/dist/lab5.js
+++ Typescript/lab5/dist/lab5.js
@@ -1,31 +1,36 @@
-// class MyClass{
-//     data: number[] = []
-//     record = (param: number) => {
-//         this.data.push(param);
-//         console.log('Record: ' + param + ' added!');
-//         setTimeout(() => {
-//             this.remove();
-//         }, 10000);
-//     }
-//     remove() {
-//     this.data.pop();
-//     console.log('Record removed!');
-//     }
-// }
-// const TestClass = new MyClass();
-// let secondsPassed = 0;
-// const intervalId = setInterval(() => {
-//   if (secondsPassed < 60) {
-//     TestClass.record(12345); 
-//     secondsPassed += 10;
-//   } else {
-//     clearInterval(intervalId); 
-//   }
-// }, 10000);
+//1
+class MyClass {
+    constructor() {
+        this.data = [];
+        this.record = (param) => {
+            this.data.push(param);
+            console.log('Record: ' + param + ' added!');
+            setTimeout(() => {
+                this.remove();
+            }, 10000);
+        };
+    }
+    remove() {
+        this.data.pop();
+        console.log('Record removed!');
+    }
+}
+const TestClass = new MyClass();
+let secondsPassed = 0;
+const intervalId = setInterval(() => {
+    if (secondsPassed < 60) {
+        TestClass.record(12345);
+        secondsPassed += 10;
+    }
+    else {
+        clearInterval(intervalId);
+    }
+}, 10000);
+//2
 function Message(message) {
     function displayMessage(message) {
         console.log(message);
     }
     displayMessage(message);
 }
-Message("Hello, Cabrones!");
+Message("Hello, World!");
